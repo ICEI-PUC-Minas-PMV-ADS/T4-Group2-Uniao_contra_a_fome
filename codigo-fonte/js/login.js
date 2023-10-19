c8a420fd203e47e8afe02b4057da0ad7d1fece83
Lucas <lucaseduardolema@gmail.com>
2023-10-19 09:54:40 -0300

    feat: add mail validation

--- codigo-fonte/js/login.js
+++ codigo-fonte/js/login.js
@@ -1,9 +1,15 @@
 function entrarBtn(e) {
   const email = document.getElementById("typeEmailX-2")
   const senha = document.getElementById("typePasswordX-2")
+  const regexValidMail = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
 
-  if (!email.value || !senha.value) {
-    alert("Email ou Senha precisam ser informados")
+  if (!regexValidMail.test(email.value)) {
+    email.value = ""
+    senha.value = ""
+    alert("Email inválido")
+    return
+  } else if (!senha.value) {
+    alert("Senha precisa ser informada")
   } else {
     window.location.href = "../pages/principal.html"
   }
