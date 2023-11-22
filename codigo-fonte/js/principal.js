938fd71799b880d0ed1dcdb2582158a114ac8211
Lucas <lucaseduardolema@gmail.com>
2023-11-22 18:35:25 -0300

    feat: add principarl.html content and principal.js fetch function

--- codigo-fonte/js/principal.js
+++ codigo-fonte/js/principal.js
@@ -1,5 +1,5 @@
 async function fetchCompanies() {
-  const response = fetch('https://fakerapi.it/api/v1/companies?_quantity=5')
+  const response = fetch('https://fakerapi.it/api/v1/companies?_quantity=15')
     .then((data) => data.json())
     .catch((error) => console.error(error))
 
